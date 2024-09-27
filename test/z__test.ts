629c434acd5b469db9b40b579b63a5bfa0a77234
Thuku <xthukuh@gmail.com>
2024-09-28 01:01:51 +0300

    calc age tests - PASS

--- test/z__test.ts
+++ test/z__test.ts
@@ -71,6 +71,20 @@ const _elapsed = (_start: any, _end: any, expects?: {years: number, months: numb
 	console.log({hours, minutes, seconds, milliseconds});
 	
 	// difference: years, months, days
+	const _dump = (tag: string, val: {days:number,months:number,years:number}, extra?: any) => {
+		const buffer: string[] = [];
+		let pass: any = expects ? true : null;
+		for (const [k, v] of Object.entries(val)){
+			const x = (expects as any)?.[k];
+			if (x !== v){
+				if (pass) pass = false;
+				buffer.push(`${k}: (${v} <> ${x} ~ ${x - v})`);
+			}
+			else buffer.push(`${k}: ${v}`);
+		}
+		const log = `${tag}[${pass ? 'PASS' : 'FAIL'}] {${buffer.join(', ')}}`;
+		console.log(...[log].concat(extra ? [extra] : []));
+	};
 	// ['1998-02-22', '2008-05-19', { years: 10, months: 2, days: 27 }],
 	// ['2004-05-31', '2005-03-01', { years: 0, months: 9, days: 1 }],
 	// ['2000-02-29', '2001-02-28', { years: 1, months: 0, days: 0 }],
@@ -78,63 +92,142 @@ const _elapsed = (_start: any, _end: any, expects?: {years: number, months: numb
 	// ['2004-05-28', '2005-03-01', { years: 0, months: 9, days: 1 }],
 	// ['2004-05-29', '2005-03-01', { years: 0, months: 9, days: 1 }],
 	// ['2004-05-30', '2005-03-01', { years: 0, months: 9, days: 1 }],
+	// ---------------------------------------------------------------
+	// Jan Feb   Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+	// 0   1     2   3   4   5   6   7   8   9   10  11
+	// 31  28/29 31  30  31  30  31  31  30  31  30  31
+	// ---------------------------------------------------------------
 	let y1 = d1.getFullYear(), m1 = d1.getMonth(), dd1 = d1.getDate();
 	let y2 = d2.getFullYear(), m2 = d2.getMonth(), dd2 = d2.getDate();
-	if (m1 === m2 && m1 === 1 && dd1 === 29 && dd2 === 28 && dd2 === new Date(y2, 2, 0).getDate()) dd1 = 28;
+	console.log('[i] ', {y1, m1, dd1});
+	console.log('[i] ', {y2, m2, dd2});
+	if (m1 === m2 && m1 === 1 && dd1 === 29 && dd2 === 28 && dd2 === new Date(y2, 2, 0).getDate()){
+		dd1 = 28;
+		console.log('[Feb-std] ', {odd1: d1.getMonth(), dd1, dd2});
+	}
+	if (y2 > y1){
+		if (m2 > m1){
+			if (dd1 === 1){
+				days = dd2 - 1;
+				months = m2 - m1;
+				years = y2 - y1;
+				_dump('[m2 > m1](dd1 === 1) ---', {days, months, years});
+			}
+			else if (dd2 > dd1){
+				days = dd2 - dd1;
+				months = m2 - m1;
+				years = y2 - y1;
+				_dump('[m2 > m1](dd2 > dd1) ---', {days, months, years});
+			}
+			else if (dd2 < dd1){
+				let dp = Math.floor((new Date(y1, m1 + 1, 1).getTime() - d1.getTime())/DAY_MS);
+				let d = Math.floor((new Date(y2, m1 + 1, 1).getTime() - new Date(y2, m1, dd1).getTime())/DAY_MS);
+				const dx = dd2 > 1 ? dd2 - 1 : 0;
+				console.log('#[m2 > m1](dd2 < dd1)[0]', {dp, d, dx, ddx: d + dx});
+				d = dx + d;
+				if ((m1 < 1 || m1 === 1 && dd1 < 28) && !(y1 % 4)){
+					if (m2 > 1 || m2 === 1 && dd2 === 29){
+						const n = !(y2 % 4) ? 0 : -1;
+						d += n;
+						console.log('##[m2 > m1](dd2 < dd1)[1]', {d, n});
+					}
+				}
+				else if ((m2 > 1 || m2 === 1 && dd2 === 29) && !(y2 % 4)) d ++;
+				console.log('###[m2 > m1](dd2 < dd1)[1]', {d});
+				days = d;
+				if ((months = m2 - new Date(y1, m1 + 1, 1).getMonth()) < 0){
+					y2 --;
+					months += 12;
+				}
+				years = y2 - y1;
+				_dump('[m2 > m1](dd2 < dd1) ---', {days, months, years});
+			}
+			else {
+				days = 0;
+				months = m2 - m1;
+				years = y2 - y1;
+				_dump('[m2 > m1](dd2 === dd1) ---', {days, months, years});
+			}
+		}
+		else if (m2 < m1){
+			if (dd1 === 1){
+				years = y2 - (y1 + 1);
+				months = 12 - m1 + m2;
+				days = dd2 - 1;
+				_dump('[m2 < m1](dd1 === 1) ---', {days, months, years});
+			}
+			else if (dd2 > dd1){
+				years = y2 - (y1 + 1);
+				months = 12 - m1 + m2;
+				days = dd2 - dd1;
+				_dump('[m2 < m1](dd2 > dd1) ---', {days, months, years});
+			}
+			else if (dd2 < dd1){
+				const e1 = new Date(y1, m1, 0).getDate();
+				const e2 = new Date(y2, m2, 0).getDate();
+				let d = 0;
+				let dp = Math.floor((new Date(y1, m1 + 1, 1).getTime() - d1.getTime())/DAY_MS);
+				if (dd1 > e1 && dd1 > e2) d = 1;
+				else if (dd1 > e2) d = (e1 - dd1) || 1;
+				else d = (e2 - dd1) || 1;
+				const dx = dd2 > 1 ? dd2 - 1 : 0;
+				console.log('@[m2 < m1](dd2 < dd1)[0]', {hh: dd1 > e2, e1, e2, dp, d, dx, ddx: d + dx});
+				d = dx + d;
+				if ((m1 < 1 || m1 === 1 && dd1 < 28) && !(y1 % 4)){
+					if (m2 > 1 || m2 === 1 && dd2 === 29){
+						const n = !(y2 % 4) ? 0 : -1;
+						d += n;
+						console.log('@@[m2 < m1](dd2 < dd1)[1]', {d, n});
+					}
+				}
+				else if ((m2 > 1 || m2 === 1 && dd2 === 29) && !(y2 % 4)) d ++;
+				console.log('@@@[m2 < m1](dd2 < dd1)[1]', {d});
+				days = d;
+				if ((months = m2 - new Date(y1, m1 + 1, 1).getMonth()) < 0){
+					y2 --;
+					months += 12;
+				}
+				years = y2 - y1;
+				_dump('[m2 < m1](dd2 < dd1) ---', {days, months, years});
+			}
+			else {
+				days = 0;
+				months = 12 - m1 + m2;
+				years = y2 - (y1 + 1);
+				_dump('[m2 < m1](dd2 === dd1) ---', {days, months, years});
+			}
+		}
+		else {
+			if (dd2 >= dd1){
+				years = y2 - y1;
+				months = 0;
+				days = dd2 - dd1;
+				_dump('[m2 === m1](dd2 >= dd1) ---', {days, months, years});
+			}
+			else {
+				// 1, 10 = 10 - 1 = 9;
+				let d = new Date(y1, m1 + 1, 0).getDate() - dd1;
+				let dd = 31 + dd2 - dd1;
+				console.debug('[m2 === m1](dd2 < dd1)[0]',{d, dd, mm: 11, days: d + dd2});
+				months = 12 - new Date(y1, m1 + 1, 1).getMonth() + m2;
+				years = y2 - (y1 + 1);
+				days = d + dd2;
+				_dump('[m2 === m1](dd2 < dd1) ---', {days, months, years});
+			}
+		}
+	}
+	else {
+		years = 0;
+		months = m2 - m1;
+		days = dd2 - dd1;
+		_dump('[y1 === y2] ---', {days, months, years});
+	}
+	/*
+	// -------------------------------------------------------------------------------------------------------
 	if (m1 === m2){
 		days = dd2 - dd1;
 		years = y2 - y1;
 	}
-	//==========================
-	// else {
-	// 	if (dd2 < dd1){
-	// 		months --;
-	// 		const leap1 = !(y1 % 4) && (m1 < 1 || m1 === 1 && dd1 < 29);
-	// 		const leap2 = !(y2 % 4) && (m2 > 1 || m2 === 1 && dd2 === 29);
-	// 		const end1 = new Date(y1, m1 + 1, 0).getDate();
-	// 		if (dd1 === end1) days = dd2 - 1 || 1;
-	// 		else if (dd2 === 1){
-	// 			days = end1 - dd1 || 1;
-	// 			console.log({dd: end1 - dd1, end1, dd1, days, m1});
-	// 		}
-	// 		else {
-	// 			let d = dd2, m = 0;
-	// 			while (d < dd1){
-	// 				const dt = new Date(y1, m2 - m, 0);
-	// 				let c = dt.getDate();
-	// 				// if (dt.getMonth() === 1 && leap1 && c < 29) c = 29;
-	// 				d += c;
-	// 				m ++;
-	// 			}
-	// 			const diff = d - dd1;
-	// 			const prev_end = new Date(y2, m2, 0).getDate();
-	// 			if ((diff === prev_end || m2 === 2 && diff === 28 || diff === 29)){
-	// 				days = 1;
-	// 				months ++;
-	// 			}
-	// 			else days = new Date(y2, m2 - m, diff).getDate();
-	// 			console.log('[~] ', {dd2, dd1, d, m, diff, days, prev_end, m2});
-	// 		}
-	// 		// if (leap2 && !leap1 && (m1 < 1 || m1 === 1 && dd1 < 29)){
-	// 		// if (leap2 && !leap1){
-	// 		// 	const days1 = days;
-	// 		// 	days ++;
-	// 		// 	console.log('[+leap] ', {days1, days});
-	// 		// }
-	// 	}
-	// 	else {
-	// 		days = dd2 - dd1;
-	// 		const prev_end = new Date(y2, m2, 0).getDate();
-	// 		console.log('[x] ', {days, prev_end});
-	// 		if ((days === prev_end || m2 === 2 && days === 28 || days === 29)) days = 1;
-	// 	}
-	// 	if ((months += m2 - m1) < 0){
-	// 		months += 12;
-	// 		y2 --;
-	// 	}
-	// 	years = y2 - y1;
-	// }
-	//==========================
 	else {
 		if (dd2 < dd1){
 			let m = 1
@@ -242,6 +335,8 @@ const _elapsed = (_start: any, _end: any, expects?: {years: number, months: numb
 		}
 		years = y2 - y1;
 	}
+	// -------------------------------------------------------------------------------------------------------
+	*/
 	return {years, months, days, hours, minutes, seconds, milliseconds};
 };
 
